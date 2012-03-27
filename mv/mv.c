ee07907ee64f01d6a50f7a0309ee16f4a4927438
niruiyu <niruiyu@98faeb64-521c-4ab6-bae1-df9f193a316f>
2012-03-27 01:12:24 +0000

    Use : to catch/get the correct driver name and handle the long driver name
    Signed-off-by: jjin9
    Reviewed-by: niruiyu

--- mv/mv.c
+++ mv/mv.c
@@ -1,6 +1,6 @@
 /*++
 
-Copyright (c) 2005 - 2006, Intel Corporation                                                         
+Copyright (c) 2005 - 2012, Intel Corporation                                                         
 All rights reserved. This program and the accompanying materials                          
 are licensed and made available under the terms and conditions of the BSD License         
 which accompanies this distribution. The full text of the license may be found at         
@@ -322,7 +322,7 @@ Returns:
       goto Done;
     }
   } else {
-    DstDevName[Index] = 0;
+    *Ptr              = 0;
     DstDevicePath     = ShellGetMap (DstDevName);
     if (!DstDevicePath) {
       PrintToken (STRING_TOKEN (STR_MV_DEST_NOT_MAPPED), HiiHandle, L"mv");
